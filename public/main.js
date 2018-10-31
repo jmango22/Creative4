8be42fd830260a8a039d3302fe6dee6d204c131b
EC2 Default User <ec2-user@ip-172-31-6-144.us-west-2.compute.internal>
2018-10-31 08:37:42 +0000

    Finished front end

--- public/main.js
+++ public/main.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     const url = "/getcity?q="+$("#cityField").val();
     $.getJSON(url, function(data) {
       var everything;
-      everything = "<ul>";
+      everything = "<ul class=\"list-group\">";
       $.each(data, function(i,item) {
-        everything += "<li> "+data[i].city;
-      })
-      
+        if (item.city !== "") {
+          everything += "<li class=\"list-group-item\">"+data[i].city +"</li>";
+        }
+      });
       everything += "</ul>";
       $("#txtHint").html(everything);
     }).done(function() { console.log('getJSON request succeeded!'); })
@@ -24,42 +25,71 @@ $(document).ready(function() {
     $("#displayCity").text(value);
     const APIKEY = "e99bfbd114fcdfd5a5a9b7547033a197";
     var myurl= `https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${APIKEY}&q=${value}`;
-    console.log(myurl);
     $.ajax({
       url : myurl,
       dataType : "json",
         success : function(parsed_json) {
-          console.log("Parsed in JSON")
           var location = parsed_json['name'];
-          var weather = parsed_json['weather'][0]['main'];
           var temp = parsed_json['main']['temp'];
-          var weather_icon = parsed_json['weather'][0]['icon'];
-          var humidity = parsed_json['main']['humidity'];
           var temp_min = parsed_json['main']['temp_min'];
           var temp_max = parsed_json['main']['temp_max'];
+          var weather = `Weather: ${parsed_json['weather'][0]['main']} (${temp}&#8457;)`;
+          var weather_icon = `http://openweathermap.org/img/w/${parsed_json['weather'][0]['icon']}.png`;
+          var humidity = parsed_json['main']['humidity'];
+          
           var wind_speed = parsed_json['wind']['speed'];
-          everything = "<img src=\"http://openweathermap.org/img/w/" + weather_icon + ".png\"/>"
-          everything += "<ul>";
-          everything += "<li>Location: " + location;
-          everything += "<li>Weather: " + weather + " (" + temp + "&#8457;)";
-          everything += "<li>Low: " + temp_min + "&#8457; | High: " + temp_max + "&#8457;";
-          everything += "<li>Humidity: " + humidity + "%";
-          everything += "<li>Wind: " + wind_speed + " mph";
-          everything += "</ul>";
-          $("#weather").html(everything);
+          let result = `
+          <div class="card">
+            <div class="card-body container">
+              <div class="left-icon">
+                <img id="weatherIcon" src="${weather_icon}" class="card-img-top" alt="Card image cap">
+              </div>
+              <div class="right-icon">
+                <h5 class="card-title">${location}</h5>
+                <h6 class="card-title">${weather}</h6>
+                <h6 class="card-title">${`Low: ${temp_min}&#8457; | High: ${temp_max}&#8457;`}</h6>
+                <h6 class="card-title">${`Humidity: ${humidity}%`}</h6>
+                <h6 class="card-title">${`Wind: ${wind_speed} mph`}</h6>
+              </div>
+            </div>
+          </div>
+          `;
+          $('#weather').html(result);
         }
     });
     e.preventDefault();
   });
+  
   $("#submitStack").click(function(e) {
+    stackExchangeSearch();
+    e.preventDefault();
+  });
+  
+  $("#stackInput").keyup(function(e) {
+    if(e.which === 13) {
+      stackExchangeSearch();
+    }
+  });
+  
+  $("#owlSearch").click(function(e) {
+    owlSearch();  
+    e.preventDefault();
+  });
+  
+  $("#owlInput").keyup(function(e) {
+    if(e.which === 13) {
+      owlSearch();
+    }
+  });
+  
+  function stackExchangeSearch() {
     const value = $("#stackInput").val();
     const url = `https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=${value}&site=stackoverflow`
     $.getJSON(url, function(data) {
-      console.log(JSON.stringify(data));
       var everything;
-      everything = "<ul>";
+      everything = "<ul class=\"list-group\">";
       $.each(data.items, function(i,item) {
-        everything += "<li><a href='"+data.items[i].link + "'>" + data.items[i].title + "</a></li>";
+        everything += "<li class=\"list-group-item\"><a href='"+data.items[i].link + "'>" + data.items[i].title + "</a></li>";
       })
       
       everything += "</ul>";
@@ -71,6 +101,33 @@ $(document).ready(function() {
     })
     .always(function() { console.log('getJSON request ended!');
     });
-    e.preventDefault();
-  });
+  }
+  
+  function owlSearch() {
+    const url = "/owlbot?q="+$("#owlInput").val().toLowerCase();
+    $.getJSON(url, function(data) {
+      if(data.length > 0) {
+        console.log(data);
+        let result = `<ul class="list-group">`;
+        for(let i=0; i<data.length; i++) {
+          const definition = data[i];
+          console.log(definition);
+          result += `
+            <li class="list-group-item">
+              <h6>${`<strong>Type:</strong> ${definition.type}`}</h6>
+              <h6>${`<strong>Definition:</strong> ${definition.defenition}`}</h6>
+              <h6>${`<strong>Example:</strong> ${definition.example}`}</h6>
+            </li>`
+        }
+        result += `</ul>`;
+        $("#owlDefinition").html(result);
+      }
+    }).done(function() { console.log('getJSON request succeeded!'); })
+    .fail(function(jqXHR, textStatus, errorThrown) { 
+      console.log('getJSON request failed! ' + textStatus); 
+      console.log("incoming "+jqXHR.responseText);
+    })
+    .always(function() { console.log('getJSON request ended!');
+    });
+  }
 });
