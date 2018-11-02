99e9a79431e53971aa2ab28ccd320324497095c8
EC2 Default User <ec2-user@ip-172-31-6-144.us-west-2.compute.internal>
2018-11-02 10:01:45 +0000

    final form

--- public/main.js
+++ public/main.js
@@ -2,44 +2,117 @@
 // When the user clicks the marker, an info window opens.
 
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  const eastern_us = {lat: 37.06173056977671, lng: -6.35018333335006};
+  var bounds = new google.maps.LatLngBounds(); // fit all the points of the map
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: uluru
-  });
-
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-    title: 'Uluru (Ayers Rock)'
+    center: eastern_us
   });
   
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
+  let outcomes = {};
+  
+  fetch('/battles')
+    .then(response => response.json())
+    .then(json => {
+      const battles = json;
+      let infoWindows = [];
+      for(let i=0; i<battles.length; i++) {
+        const battle = battles[i];
+        const point = new google.maps.LatLng(battle.battle_location);
+        bounds.extend(point);
+        
+        
+        const template = `
+        <div id="content">
+          <div id="siteNotice"></div>
+          <h1 id="firstHeading" class="firstHeading">
+            ${battle.name}
+          </h1>
+          <div id="bodyContent">
+            <p>
+              ${battle.content}
+            </p>
+            <p>Attribution: ${battle.name}, 
+              <a href=${battle.article}>
+                ${battle.article}
+              </a>
+            </p>
+          </div>
+        </div>`;
+        
+        const infoWindow = new google.maps.InfoWindow({
+          content: template
+        });
+        
+        infoWindows.push(infoWindow);
+      
+        const image = getIcon(battle.outcome);
+        
+        let marker;
+        if(image) {
+          marker = new google.maps.Marker({
+            position: battle.battle_location,
+            map: map,
+            title: battle.name,
+            icon: image
+          });
+        } else {
+          marker = new google.maps.Marker({
+            position: battle.battle_location,
+            map: map,
+            title: battle.name
+          });
+        }
+        
+        marker.addListener('click', function() {
+          for(let i=0; i<infoWindows.length; i++) {
+            infoWindows[i].close();
+          }
+          infoWindow.open(map, marker);
+        });
+      }
+      map.fitBounds(bounds);
+    });
+}
+
+tippy('.about', {
+  content: `
+    <p>
+      This map shows every battle in the Revolutionary War. Each marker represents 
+      the country that won the battle. Generic marker mean there was a draw.
+      Clicking on a marker displays information about the battle, including a link
+      to the battle's wikipedia page.
+    </p>`
+});
+
+// Custom icons for each country involved
+const icons = {
+  british: 'britain.svg',
+  patriot: 'usa.svg',
+  spanish: 'spain.svg',
+  french: 'france.svg',
+  loyalist: 'britain.svg'
+}
+
+function getIcon(outcome) {
+  const groups = Object.keys(icons);
+  for(let i=0; i<groups.length; i++) {
+    if(isGroupVictory(outcome, groups[i])) {
+      const image = {
+        url: icons[groups[i]],
+        scaledSize: new google.maps.Size(35,40)
+      }
+      return image;
+    }
+  }
+  return null;
+}
+
+function isGroupVictory(outcome, group) {
+  if((outcome.toLowerCase().indexOf(`${group} victory`) !== -1) 
+    || (outcome.toLowerCase().indexOf(`${group}s victory`) !== -1)
+    || (outcome.toLowerCase().indexOf(group) === 0)) {
+    return true;
+  } 
+  return false;
 }
